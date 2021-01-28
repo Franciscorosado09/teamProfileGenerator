30bb292ba47885becd30b711de5b93513ca839b3
Francisco Rosado <Franciscorosado09@gmail.com>
2021-01-27 22:40:15 -0500

    added first class

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamProfiles = []
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -33,3 +35,20 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// const addTeamMember = ()=>{
+// console.log ("hello world")
+// inquirer.prompt([
+
+//     {
+//         type: "list",
+//         name: "type",
+//         message: "Which type of employee are you entering into the system?",
+//         choices: ["Manager", "Engineer", "Intern", "None"],
+//     }
+
+// ])
+
+// }
+
+//
